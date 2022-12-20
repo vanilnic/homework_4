d7b227d4abe0f44f6f28706de05f4ee1d436c0f8
vanilnic <bikadog306@gmail.com>
2022-12-20 16:38:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,27 +2,25 @@
 
 document.getElementById("student").innerHTML = student;
 
+const errorRate = 0.2;
 
 function getRandomIntInclusive(min, max) {
- min = Math.ceil(min);
- max = Math. floor(max);
+  min = Math.ceil(min);
+  max = Math.floor(max);
  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
- function throwRandomError(errorRate) {
-   if (getRandomIntInclusive(0, 20) <= errorRate) {
-     throw new Error('Ой, что-то сломалось');
  }
+  function throwRandomError(errorRate) {
+ 
+   if (getRandomIntInclusive(0.0, 1.0) <= errorRate) {
+      throw new Error('Ой, что-то сломалось');
+}
 }
 
-
- try {
-
-throwRandomError(errorRate) ;
-
-console.log('Всё в порядке, приложение работает в штатном режиме');
-
-} catch (err) {
- console. log('Произошла ошибка, пожалуйста перезагрузите страницу');
-
-console. log(err);
-}
+try {
+  throwRandomError(errorRate) ;
+  console.log('Всё в порядке, приложение работает в штатном режиме');
+  
+  } catch (err) {
+  console.log('Произошла ошибка, пожалуйста перезагрузите страницу');
+  console.log(err);
+  }
