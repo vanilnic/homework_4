54bb16f9238ff259d36d92eebcd240eb8923e418
vanilnic <bikadog306@gmail.com>
2022-12-21 22:40:15 +0300

    hw4

---
+++ script.js
@@ -0,0 +1,29 @@
+// const student = "Богданова Виктория Алексеевна"; // Очевидно, что здесь ваши личные Фамилия, Имя и Отчество
+
+document.getElementById("student").innerHTML = student;
+
+
+function getRandomIntInclusive(min, max) {
+ min = Math.ceil(min);
+ max = Math. floor(max);
+ return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+ function throwRandomError(errorRate) {
+   if (getRandomIntInclusive(0, 100) <= errorRate) {
+     throw new Error('Ой, что-то сломалось');
+ }
+}
+
+let errorRate = +prompt('Введите вероятность выпада ошибки в процентах', 20)
+
+ try {
+
+throwRandomError(errorRate) ;
+
+console.log('Всё в порядке, приложения работает в штатном режиме');
+
+} catch (err) {
+ console. log('Произошла ошибка, пожалуйста перезагрузите страницу');
+
+console. log(err);
+}
